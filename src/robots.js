da2529a8ceff0dd7a0ac875305e76ae63c5fd9ab
Ernesto Trujillo <ernesto25trujillo@hotmail.com>
2019-11-21 19:31:14 -0500

    Project Imported

---
+++ src/robots.js
@@ -0,0 +1,60 @@
+export const robots = [
+
+{
+	id: 1,
+	name: 'Ernesto Trujillo',
+	username: 'HeyErnesto',
+	email: 'ernesto25trujillo@hotmail.com'
+},
+{
+	id: 32,
+	name: 'Angie Mendoza',
+	username: 'pao',
+	email: 'angieguati004@gmail.com'
+},
+{
+	id: 4,
+	name: 'Angie Mendoza',
+	username: 'pao',
+	email: 'angieguati004@gmail.com'
+},
+{
+	id: 7,
+	name: 'Angie Mendoza',
+	username: 'pao',
+	email: 'angieguati004@gmail.com'
+},
+{
+	id: 89,
+	name: 'Angie Mendoza',
+	username: 'pao',
+	email: 'angieguati004@gmail.com'
+},
+{
+	id: 25,
+	name: 'Angie Mendoza',
+	username: 'pao',
+	email: 'angieguati004@gmail.com'
+},
+{
+	id: 8,
+	name: 'Angie Mendoza',
+	username: 'pao',
+	email: 'angieguati004@gmail.com'
+},
+{
+	id: 13,
+	name: 'Angie Mendoza',
+	username: 'pao',
+	email: 'angieguati004@gmail.com'
+},
+{
+	id: 11,
+	name: 'Angie Mendoza',
+	username: 'pao',
+	email: 'angieguati004@gmail.com'
+}
+
+
+
+]
